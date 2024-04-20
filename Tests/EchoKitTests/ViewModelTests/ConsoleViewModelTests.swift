53d431cc1c8d8b036ca830cf8a59522f0c65fc67
Lukas <KasRoid@gmail.com>
2024-04-20 11:48:39 +0900

    [test] Add unit tests for ConsoleViewModel's isActivePublisher

--- Tests/EchoKitTests/ViewModelTests/ConsoleViewModelTests.swift
+++ Tests/EchoKitTests/ViewModelTests/ConsoleViewModelTests.swift
@@ -5,18 +5,35 @@
 //  Created by Doyoung Song on 4/19/24.
 //
 
+import Combine
 import XCTest
 @testable import EchoKit
 
 final class ConsoleViewModelTests: XCTestCase {
 
     private var sut: ConsoleViewModel!
+    private var subject = PassthroughSubject<Bool, Never>()
+    private var cancellables = Set<AnyCancellable>()
 
     override func setUpWithError() throws {
-        sut = ConsoleViewModel()
+        sut = ConsoleViewModel(publisher: subject.eraseToAnyPublisher())
     }
 
     override func tearDownWithError() throws {
         sut = nil
+    }
+    
+    func testIsActivePublisherReceivesUpdate() {
+        var sendingValues = [true, false]
+        var receivedValues: [Bool] = []
+        
+        sut.isActivePublisher
+            .sink { receivedValues.append($0) }
+            .store(in: &cancellables)
+        
+        for value in sendingValues {
+            subject.send(value)
+        }
+        XCTAssertEqual(sendingValues, receivedValues)
     }
 }
