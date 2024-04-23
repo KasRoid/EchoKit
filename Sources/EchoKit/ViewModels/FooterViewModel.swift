09b1de001a91ad00d1023c8a24feab91dd8f0fe7
Lukas <KasRoid@gmail.com>
2024-04-23 13:17:34 +0900

    [feat] Implement log counting function

--- Sources/EchoKit/ViewModels/FooterViewModel.swift
+++ Sources/EchoKit/ViewModels/FooterViewModel.swift
@@ -5,6 +5,23 @@
 //  Created by Lukas on 4/19/24.
 //
 
-import Foundation
+import Combine
 
-internal final class FooterViewModel {}
+internal final class FooterViewModel {
+    
+    @Published private(set) var count = 0
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        bind()
+    }
+}
+
+// MARK: - Bindings
+extension FooterViewModel {
+    
+    private func bind() {
+        cancellable = Buffer.shared.$logs
+            .sink { [weak self] in self?.count = $0.count }
+    }
+}
--- Sources/EchoKit/Views/FooterView.swift
+++ Sources/EchoKit/Views/FooterView.swift
@@ -5,16 +5,20 @@
 //  Created by Lukas on 4/19/24.
 //
 
+import Combine
 import UIKit
 
 internal final class FooterView: UIView {
     
+    @IBOutlet private weak var logCountLabel: UILabel!
     private var viewModel: FooterViewModel!
+    private var cancellables = Set<AnyCancellable>()
     
     init(viewModel: FooterViewModel) {
         self.viewModel = viewModel
         super.init(frame: .zero)
         setupWithXib()
+        bind()
     }
     
     required init?(coder: NSCoder) {
@@ -28,5 +32,16 @@ extension FooterView {
     
     internal func prepare(viewModel: FooterViewModel) {
         self.viewModel = viewModel
+        bind()
+    }
+}
+
+// MARK: - Bindings
+extension FooterView {
+    
+    private func bind() {
+        viewModel.$count
+            .sink { [weak self] in self?.logCountLabel.text = "\($0)L" }
+            .store(in: &cancellables)
     }
 }
